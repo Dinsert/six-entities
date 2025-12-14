3ed6b95b595ed6de36f26c4a9e028371e00ebc8a
Dinsert <korolkov.01@bk.ru>
2025-12-14 19:37:57 +0300

    Initial commit

---
+++ src/main/java/com/example/six_entities/SixEntitiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.six_entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixEntitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixEntitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/six_entities/TestSixEntitiesApplication.java
@@ -0,0 +1,11 @@
+package com.example.six_entities;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSixEntitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(SixEntitiesApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
